14aca889c32bfd13a549ba1458e0621322da9c2a
fwshah <95996926+fwshah@users.noreply.github.com>
2025-07-30 08:39:22 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import streamlit as st
 import numpy as np
-from sklearn.ensemble import RandomForestClassifies
+from sklearn.ensemble import RandomForestClassifier
 
 st.title("Penguine Specy Prediction ML app")
 st.info("This is end-t-end MAchine Learning App")
