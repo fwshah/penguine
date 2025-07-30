fa61347a81a49b23ebcec398551ba114d866bfe4
fwshah <95996926+fwshah@users.noreply.github.com>
2025-07-30 16:39:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ with st.expander("Data"):
   st.write("*Raw Data*")
   df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
   df
+  
   st.write("Input Vriables")
   X_raw = df.drop("species", axis = 1)
   X_raw
