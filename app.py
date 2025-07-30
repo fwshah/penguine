948cea5da18e90fc3488e3f30b00c0e9730676fd
fwshah <95996926+fwshah@users.noreply.github.com>
2025-07-30 09:04:14 -0600

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ with st.expnder("Data"):
   st.write("**Raw data**")
   df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
   df
- st.write("Input variable")
+ st.write("Input variables")
 x_raw = df.drop('species',axis=1)
 y_raw = df.species 
 y_raw
