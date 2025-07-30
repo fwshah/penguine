cdee33d6d9a2bd25cfdafbd46d93b5dbadc09e38
fwshah <95996926+fwshah@users.noreply.github.com>
2025-07-30 09:30:42 -0600

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ with st.sidebar:
           'flipper_length_mm': flipper_length_mm,
           'gender': gender
   }
- input_df = pd.DataFrame(data, index[0])
+input_df = pd.DataFrame(data, index[0])
 input_penguins = pd.concat([input_df, x_raw], axis=0)
 with st.expandar("Input data"):
  st.write(**Input data**)
