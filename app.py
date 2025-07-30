ebf7898e3a69c9ad0fb1626af6857489ada405f1
fwshah <95996926+fwshah@users.noreply.github.com>
2025-07-30 09:10:38 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ st.title("Penguine Speicy Prediction ML app")
 st.info("This is end-to-end Machine Learning App")
  
 with st.expnder("Data"):
-st.write("**Raw data**")
-df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
-df
-st.write("Input variables")
-x_raw = df.drop('species',axis=1)
-y_raw = df.species 
-y_raw
+ st.write("**Raw data**")
+ df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+ df
+ st.write("Input variables")
+ x_raw = df.drop('species',axis=1)
+ y_raw = df.species 
+ y_raw
 
 st.write("Descriptive Statistics")
 des = df.describe()
